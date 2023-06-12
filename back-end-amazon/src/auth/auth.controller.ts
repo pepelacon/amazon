2df42ec1e8957a452df62c68b2d3bf9b1aeb44de
Daniel <pepelacon@gmail.com>
2023-06-12 19:59:43 -0400

    add auth register

--- back-end-amazon/src/auth/auth.controller.ts
+++ back-end-amazon/src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(200)
+  @Post('register')
+  async register() {
+    return this.authService.register()
+  }
 }
