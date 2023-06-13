3430ef6946df1e72f72150e1d74960643743d20f
Daniel <pepelacon@gmail.com>
2023-06-13 09:36:25 -0400

    add auth and create tokens

--- back-end-amazon/src/auth/auth.service.ts
+++ back-end-amazon/src/auth/auth.service.ts
@@ -1,11 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { faker } from '@faker-js/faker';
+import { AuthDto } from './auth.dto'
+import { hash } from 'argon2'
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  register() {
-    return {
-      name: 'Daniel'
-    }
+  constructor(private prisma: PrismaService, private jwt:
+    JwtService) {}
+
+  async register(dto: AuthDto) {
+    const oldUser = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email
+      }
+    })
+
+    if (oldUser) throw new BadRequestException('User already exist') 
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        name: faker.person.firstName(),
+        avatarPath: faker.image.avatar(),
+        phone: faker.phone.number('+1 (###) ###-##-##'),
+        password: await hash(dto.password)
+      }
+    })
+
+    return user
+  }
+
+  private async issueTokens(userId: number) {
+    const data = {id: userId}
+
+    const accessToken = this.jwt.sign(data, {
+      expiresIn: '1h',
+    })
+    const refreshToken = this.jwt.sign(data, {
+      expiresIn: '7d',
+    })
+
+    return { accessToken, refreshToken }
   }
 }
 
